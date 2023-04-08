79099d1d78b1dfe31cc3fddbd0a7543698deb4ba
Maciej Stępień <maciek1284@outlook.com>
2023-04-08 21:22:18 +0000

    Add combined launch

---
+++ launch/simulation_with_controller.launch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import (
+    PathJoinSubstitution,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    simulation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("furuta_pendulum"),
+                    "launch",
+                    "simulation.launch.py",
+                ]
+            )
+        ),
+    )
+    controller_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("furuta_pendulum"),
+                    "launch",
+                    "controller.launch.py",
+                ]
+            )
+        ),
+    )
+    actions = [simulation_launch, controller_launch]
+
+    return LaunchDescription(actions)
