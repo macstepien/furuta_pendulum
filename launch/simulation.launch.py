0ba5cca66aadb8ab6f18b41bc93deb5549621342
Maciej Stępień <maciek1284@outlook.com>
2023-04-09 13:43:35 +0000

    Add simulation properties to urdf

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python3
 
+import os
+import yaml
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.substitutions import (
     Command,
     FindExecutable,
     PathJoinSubstitution,
-    LaunchConfiguration,
 )
 
-from launch_ros.actions import Node, SetParameter
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
+def load_yaml(package_name, directory_name, file_name):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, directory_name, file_name)
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:
+        return None
+
+
 def generate_launch_description():
+    pendulum_parameters = load_yaml(
+        "furuta_pendulum", "config", "pendulum_parameters.yaml"
+    )["/**"]["ros__parameters"]
+
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
@@ -25,6 +41,26 @@ def generate_launch_description():
                     "furuta_pendulum.urdf.xacro",
                 ]
             ),
+            " m1:=",
+            str(pendulum_parameters["m1"]),
+            " m2:=",
+            str(pendulum_parameters["m2"]),
+            " l1:=",
+            str(pendulum_parameters["l1"]),
+            " l2:=",
+            str(pendulum_parameters["l2"]),
+            " L1:=",
+            str(pendulum_parameters["L1"]),
+            " L2:=",
+            str(pendulum_parameters["L2"]),
+            " J1:=",
+            str(pendulum_parameters["J1"]),
+            " J2:=",
+            str(pendulum_parameters["J2"]),
+            " b1:=",
+            str(pendulum_parameters["b1"]),
+            " b2:=",
+            str(pendulum_parameters["b2"]),
         ]
     )
     robot_description = {"robot_description": robot_description_content}
