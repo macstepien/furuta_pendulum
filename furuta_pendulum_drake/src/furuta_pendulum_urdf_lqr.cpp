12c7fa0771d8c8716d8be9e58832528d3937796c
Maciej Stępień <maciek1284@outlook.com>
2023-04-10 16:14:26 +0000

    Switch to meshcat and refactor

--- furuta_pendulum_drake/src/furuta_pendulum_urdf_lqr.cpp
+++ furuta_pendulum_drake/src/furuta_pendulum_urdf_lqr.cpp
@@ -3,7 +3,7 @@
 #include <drake/systems/framework/event.h>
 #include <drake/common/drake_assert.h>
 #include <drake/common/find_resource.h>
-#include <drake/geometry/drake_visualizer.h>
+#include <drake/geometry/meshcat_visualizer.h>
 #include <drake/geometry/scene_graph.h>
 #include <drake/lcm/drake_lcm.h>
 #include <drake/multibody/parsing/parser.h>
@@ -43,31 +43,32 @@ void DoMain()
   scene_graph.set_name("scene_graph");
 
   // Load and parse furuta pendulum URDF from file into a tree.
-  drake::multibody::MultibodyPlant<double> * cp =
+  drake::multibody::MultibodyPlant<double> * furuta_pendulum_plant =
     builder.AddSystem<drake::multibody::MultibodyPlant<double>>(kMaxTimeStep);
-  cp->set_name("furuta_pendulum");
-  cp->RegisterAsSourceForSceneGraph(&scene_graph);
+  furuta_pendulum_plant->set_name("furuta_pendulum");
+  furuta_pendulum_plant->RegisterAsSourceForSceneGraph(&scene_graph);
 
-  drake::multibody::Parser parser(cp);
+  drake::multibody::Parser parser(furuta_pendulum_plant);
   drake::multibody::ModelInstanceIndex plant_model_instance_index =
     parser.AddModelFromFile(kFurutaPendulumUrdfPath);
   (void)plant_model_instance_index;
 
   // Now the plant is complete.
-  cp->Finalize();
+  furuta_pendulum_plant->Finalize();
 
   // Create LQR Controller.
-  auto cp_context = cp->CreateDefaultContext();
+  auto furuta_pendulum_context = furuta_pendulum_plant->CreateDefaultContext();
   const int furuta_pendulum_actuation_port = 3;
   // Set nominal torque to zero.
   Eigen::VectorXd u0 = Eigen::VectorXd::Zero(2);
-  cp_context->FixInputPort(furuta_pendulum_actuation_port, Value<systems::BasicVector<double>>(u0));
+  furuta_pendulum_context->FixInputPort(
+    furuta_pendulum_actuation_port, Value<systems::BasicVector<double>>(u0));
 
   // Set nominal state to the upright fixed point.
   Eigen::VectorXd x0 = Eigen::VectorXd::Zero(4);
-  x0[0] = 1;
+  x0[0] = 0;
   x0[1] = M_PI;
-  cp_context->SetDiscreteState(x0);
+  furuta_pendulum_context->SetDiscreteState(x0);
 
   // Setup LQR Cost matrices (penalize position error 10x more than velocity
   // to roughly address difference in units, using sqrt(g/l) as the time
@@ -78,31 +79,33 @@ void DoMain()
   Eigen::MatrixXd R = Eigen::MatrixXd::Identity(2, 2);
   Eigen::MatrixXd N;
   auto lqr = builder.AddSystem(systems::controllers::LinearQuadraticRegulator(
-    *cp, *cp_context, Q, R, N, furuta_pendulum_actuation_port));
+    *furuta_pendulum_plant, *furuta_pendulum_context, Q, R, N, furuta_pendulum_actuation_port));
 
-  builder.Connect(cp->get_state_output_port(), lqr->get_input_port());
-  builder.Connect(lqr->get_output_port(), cp->get_actuation_input_port());
+  builder.Connect(furuta_pendulum_plant->get_state_output_port(), lqr->get_input_port());
+  builder.Connect(lqr->get_output_port(), furuta_pendulum_plant->get_actuation_input_port());
 
   // Connect plant with scene_graph to get collision information.
-  DRAKE_DEMAND(!!cp->get_source_id());
+  DRAKE_DEMAND(!!furuta_pendulum_plant->get_source_id());
   builder.Connect(
-    cp->get_geometry_poses_output_port(),
-    scene_graph.get_source_pose_port(cp->get_source_id().value()));
-  builder.Connect(scene_graph.get_query_output_port(), cp->get_geometry_query_input_port());
+    furuta_pendulum_plant->get_geometry_poses_output_port(),
+    scene_graph.get_source_pose_port(furuta_pendulum_plant->get_source_id().value()));
+  builder.Connect(
+    scene_graph.get_query_output_port(), furuta_pendulum_plant->get_geometry_query_input_port());
 
-  geometry::DrakeVisualizerd::AddToBuilder(&builder, scene_graph);
+  geometry::MeshcatVisualizerd::AddToBuilder(
+    &builder, scene_graph, std::make_shared<geometry::Meshcat>());
 
   auto diagram = builder.Build();
   std::unique_ptr<systems::Context<double>> diagram_context = diagram->CreateDefaultContext();
 
   // Create plant_context to set velocity.
   systems::Context<double> & plant_context =
-    diagram->GetMutableSubsystemContext(*cp, diagram_context.get());
+    diagram->GetMutableSubsystemContext(*furuta_pendulum_plant, diagram_context.get());
   // Set init position.
   Eigen::VectorXd positions = Eigen::VectorXd::Zero(2);
   positions[0] = 0.0;
   positions[1] = 0.0;
-  cp->SetPositions(&plant_context, positions);
+  furuta_pendulum_plant->SetPositions(&plant_context, positions);
 
   systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
   simulator.set_publish_every_time_step(true);
