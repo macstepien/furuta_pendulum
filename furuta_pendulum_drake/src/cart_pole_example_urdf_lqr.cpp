98c8479983475ff5c2522df94058039a80b6fb13
Maciej Stępień <maciek1284@outlook.com>
2023-04-10 14:04:31 +0000

    Switch visualization to meshcat

--- furuta_pendulum_drake/src/cart_pole_example_urdf_lqr.cpp
+++ furuta_pendulum_drake/src/cart_pole_example_urdf_lqr.cpp
@@ -7,7 +7,7 @@
 #include <drake/systems/framework/event.h>
 #include <drake/common/drake_assert.h>
 #include <drake/common/find_resource.h>
-#include <drake/geometry/drake_visualizer.h>
+#include <drake/geometry/meshcat_visualizer.h>
 #include <drake/geometry/scene_graph.h>
 #include <drake/lcm/drake_lcm.h>
 #include <drake/multibody/parsing/parser.h>
@@ -98,7 +98,8 @@ void DoMain()
     scene_graph.get_source_pose_port(cp->get_source_id().value()));
   builder.Connect(scene_graph.get_query_output_port(), cp->get_geometry_query_input_port());
 
-  geometry::DrakeVisualizerd::AddToBuilder(&builder, scene_graph);
+  geometry::MeshcatVisualizerd::AddToBuilder(
+    &builder, scene_graph, std::make_shared<geometry::Meshcat>());
 
   auto diagram = builder.Build();
   std::unique_ptr<systems::Context<double>> diagram_context = diagram->CreateDefaultContext();
