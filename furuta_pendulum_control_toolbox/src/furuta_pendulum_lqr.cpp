95843f6326e1a66ea51592caa59360b0c583d1cb
Maciej Stępień <maciek1284@outlook.com>
2023-04-12 23:47:30 +0000

    Working lqr in control toolbox

--- furuta_pendulum_control_toolbox/src/furuta_pendulum_lqr.cpp
+++ furuta_pendulum_control_toolbox/src/furuta_pendulum_lqr.cpp
@@ -1,9 +1,6 @@
 // Based on NLOC_MPC example from control_toolbox library
 // https://github.com/ethz-adrl/control-toolbox/blob/v3.0.2/ct_models/examples/mpc/InvertedPendulum/NLOC_MPC.cpp
 
-// basically only for intellisense
-#define CPPADCG
-
 #include <ct/optcon/optcon.h>
 #include <ct/rbd/rbd.h>
 
@@ -16,48 +13,60 @@ int main()
   std::string workingDirectory =
     "/home/maciej/ros2_ws/src/furuta_pendulum/furuta_pendulum_control_toolbox/config";
 
-  const size_t njoints = ct::rbd::FurutaPendulum::Kinematics::NJOINTS;
-  const size_t actuator_state_dim = 2;
+  // obtain the state dimension
+  const size_t STATE_DIM =
+    ct::rbd::FixBaseFDSystem<ct::rbd::FurutaPendulum::tpl::Dynamics<double>>::STATE_DIM;
 
-  static const size_t state_dim = ct::rbd::FixBaseRobotState<njoints, actuator_state_dim>::NSTATE;
-  static const size_t control_dim = njoints;
+  const size_t CONTROL_DIM = 2;
 
-  const double k_spring = 160;
-  const double gear_ratio = 50;
-  std::shared_ptr<ct::rbd::SEADynamicsFirstOrder<njoints>> actuatorDynamics(
-    new ct::rbd::SEADynamicsFirstOrder<njoints>(k_spring, gear_ratio));
-
-  std::shared_ptr<ct::rbd::FixBaseFDSystem<
-    ct::rbd::FurutaPendulum::tpl::Dynamics<double>, actuator_state_dim, false>>
-    ipSystem(new ct::rbd::FixBaseFDSystem<
-             ct::rbd::FurutaPendulum::tpl::Dynamics<double>, actuator_state_dim, false>(
-      actuatorDynamics));
+  // create an instance of the system
+  std::shared_ptr<ct::core::ControlledSystem<STATE_DIM, CONTROL_DIM, ct::core::ADCGScalar>>
+    dynamics(
+      new ct::rbd::FixBaseFDSystem<ct::rbd::FurutaPendulum::tpl::Dynamics<ct::core::ADCGScalar>>);
 
   // create an Auto-Differentiation Linearizer with code generation on the quadrotor model
-  ct::core::ADCodegenLinearizer<state_dim, control_dim> adLinearizer(ipSystem);
+  ct::core::ADCodegenLinearizer<STATE_DIM, CONTROL_DIM> adLinearizer(dynamics);
   // compile the linearized model just-in-time
   adLinearizer.compileJIT();
   // define the linearization point around steady state
-  ct::core::StateVector<state_dim> x;
+  ct::core::StateVector<STATE_DIM> x;
   x.setZero();
-  ct::core::ControlVector<control_dim> u;
+  x(1) = M_PI;
+  ct::core::ControlVector<CONTROL_DIM> u;
   u.setZero();
   double t = 0.0;
   // compute the linearization around the nominal state using the Auto-Diff Linearizer
   auto A = adLinearizer.getDerivativeState(x, u, t);
   auto B = adLinearizer.getDerivativeControl(x, u, t);
+
+  std::cout << B << std::endl;
+  std::cout << B(0) << std::endl;
+  std::cout << B(0, 0) << std::endl;
+  B(2, 1) = 0.0;
+  B(3, 1) = 0.0;
+
+  Eigen::MatrixXd real_B(4, 1);
+  real_B(0, 0) = B(0, 0);
+  real_B(1, 0) = B(1, 0);
+  real_B(2, 0) = B(2, 0);
+  real_B(3, 0) = B(3, 0);
+
+  const size_t REAL_CONTROL_DIM = 1;
+  ct::core::ControlVector<REAL_CONTROL_DIM> real_u;
+  real_u.setZero();
+
   // load the weighting matrices
-  ct::optcon::TermQuadratic<state_dim, control_dim> quadraticCost;
+  ct::optcon::TermQuadratic<STATE_DIM, REAL_CONTROL_DIM> quadraticCost;
   quadraticCost.loadConfigFile(workingDirectory + "/lqr_cost.info", "termLQR");
-  auto Q = quadraticCost.stateSecondDerivative(x, u, t);    // x, u and t can be arbitrary here
-  auto R = quadraticCost.controlSecondDerivative(x, u, t);  // x, u and t can be arbitrary here
+  auto Q = quadraticCost.stateSecondDerivative(x, real_u, t);    // x, u and t can be arbitrary here
+  auto R = quadraticCost.controlSecondDerivative(x, real_u, t);  // x, u and t can be arbitrary here
   // design the LQR controller
-  ct::optcon::LQR<state_dim, control_dim> lqrSolver;
-  ct::core::FeedbackMatrix<state_dim, control_dim> K;
+  ct::optcon::LQR<STATE_DIM, REAL_CONTROL_DIM> lqrSolver;
+  ct::core::FeedbackMatrix<STATE_DIM, REAL_CONTROL_DIM> K;
   std::cout << "A: " << std::endl << A << std::endl << std::endl;
-  std::cout << "B: " << std::endl << B << std::endl << std::endl;
+  std::cout << "B: " << std::endl << real_B << std::endl << std::endl;
   std::cout << "Q: " << std::endl << Q << std::endl << std::endl;
   std::cout << "R: " << std::endl << R << std::endl << std::endl;
-  lqrSolver.compute(Q, R, A, B, K);
+  lqrSolver.compute(Q, R, A, real_B, K);
   std::cout << "LQR gain matrix:" << std::endl << K << std::endl;
 }
