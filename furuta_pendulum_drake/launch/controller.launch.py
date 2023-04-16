b56804d8d83f7699a9b00362fab7e152a9bbe8e8
Maciej Stępień <maciek1284@outlook.com>
2023-04-16 18:38:24 +0000

    Add controller launch for drake's lqr weights

---
+++ furuta_pendulum_drake/launch/controller.launch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.substitutions import (
+    PathJoinSubstitution,
+)
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    controller_params = PathJoinSubstitution(
+        [
+            FindPackageShare("furuta_pendulum_drake"),
+            "config",
+            "controller.yaml",
+        ]
+    )
+
+    pendulum_params = PathJoinSubstitution(
+        [
+            FindPackageShare("furuta_pendulum"),
+            "config",
+            "pendulum_parameters.yaml",
+        ]
+    )
+
+    furuta_pendulum_controller_node = Node(
+        package="furuta_pendulum",
+        executable="controller_node",
+        parameters=[controller_params, pendulum_params],
+    )
+
+    actions = [
+        furuta_pendulum_controller_node,
+    ]
+
+    return LaunchDescription(actions)
---
+++ furuta_pendulum_drake/launch/simulation_with_controller.launch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.substitutions import (
+    PathJoinSubstitution,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    simulation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("furuta_pendulum"),
+                    "launch",
+                    "simulation.launch.py",
+                ]
+            )
+        ),
+    )
+    controller_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("furuta_pendulum_drake"),
+                    "launch",
+                    "controller.launch.py",
+                ]
+            )
+        ),
+    )
+    actions = [simulation_launch, TimerAction(period=4.0, actions=[controller_launch])]
+
+    return LaunchDescription(actions)
