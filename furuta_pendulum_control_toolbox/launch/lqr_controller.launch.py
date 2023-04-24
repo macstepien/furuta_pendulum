b96f5e58fd02af14ba1d4933c7191d0e9b462fb1
Maciej Stępień <maciek1284@outlook.com>
2023-04-24 20:29:07 +0000

    Add ct lqr

---
+++ furuta_pendulum_control_toolbox/launch/lqr_controller.launch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.substitutions import (
+    PathJoinSubstitution,
+)
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    controller_params = PathJoinSubstitution(
+        [
+            FindPackageShare("furuta_pendulum_control_toolbox"),
+            "config",
+            "lqr_with_swingup_controller.yaml",
+        ]
+    )
+
+    pendulum_params = PathJoinSubstitution(
+        [
+            FindPackageShare("furuta_pendulum_description"),
+            "config",
+            "pendulum_parameters.yaml",
+        ]
+    )
+
+    furuta_pendulum_controller_node = Node(
+        package="furuta_pendulum_de",
+        executable="lqr_with_swingup_controller_node",
+        parameters=[controller_params, pendulum_params],
+    )
+
+    actions = [
+        furuta_pendulum_controller_node,
+    ]
+
+    return LaunchDescription(actions)
