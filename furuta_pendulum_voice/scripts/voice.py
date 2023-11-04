ff3c95b181fe5bdb8478c7e473a977f4929757f3
Maciej Stępień <maciek1284@outlook.com>
2023-11-04 09:17:19 +0000

    Refactor voice script

--- furuta_pendulum_voice/scripts/voice.py
+++ furuta_pendulum_voice/scripts/voice.py
@@ -8,10 +8,20 @@ from std_msgs.msg import Float64MultiArray
 
 import vlc
 
-from random import randint
+from random import randint, uniform
 
 from rclpy.duration import Duration
 import math
+
+from enum import Enum
+
+
+class PendulumState(Enum):
+    SWING_UP = 1
+    STABILIZING = 2
+    STABILIZED = 3
+    MOVED = 4
+
 
 class PendulumVoice(Node):
     def __init__(self):
@@ -19,6 +29,7 @@ class PendulumVoice(Node):
 
         # self.declare_parameter("retraction_height", 0.01)
         # self.retraction_height = self.get_parameter("retraction_height").value
+        # self.get_logger().info("playing")
 
         self.sub = self.create_subscription(JointState, "/joint_states", self.cb, 10)
         self.sub_cmd = self.create_subscription(
@@ -26,77 +37,94 @@ class PendulumVoice(Node):
         )
 
         self.sounds_path = "file:///home/pendulum/ros2_ws/src/sounds/"
-        # playsound(self.sounds_path + "start.mp3")
-        p = vlc.MediaPlayer(self.sounds_path + "start.mp3")
-        p.play()
+
+        self.last_played_sound = None
+        
+        self.current_sound = None
+        self.play("start")
 
         self.last_cmd = 0
         self.current_state = None
         self.last_played_stamp = self.get_clock().now()
         self.min_play_time = Duration(seconds=0.1)
 
-        self.stabilizing = False
-        self.stabilized = False
+        self.pendulum_state = PendulumState.SWING_UP
 
-    # def play(self, sound):
-    #     pass
+        self.cmd_direction_changed = False
+        self.stabilized_play_time = Duration(seconds=1.0)
 
+    def play(self, sound):
+        if self.current_sound:
+            self.current_sound.stop()
+            
+        self.last_played_stamp = self.get_clock().now()
+        self.last_played_sound = sound
+        self.current_sound = vlc.MediaPlayer(self.sounds_path + sound + ".mp3")
+        self.current_sound.play()
 
     def cmd_cb(self, msg: Float64MultiArray):
+        self.cmd_direction_changed = self.last_cmd * msg.data[0] < 0
+        self.last_cmd = msg.data[0]
+
+        self.voice()
+
+    def voice(self):
         if not self.current_state:
-            return 
+            return
 
         if self.get_clock().now() - self.last_played_stamp < self.min_play_time:
             return
-        
-        angle = math.fabs(self.current_state.position[1])
 
-        if (
-            self.last_cmd * msg.data[0] < 0
-            and angle > 0.7
+        if self.cmd_direction_changed and self.pendulum_state == PendulumState.SWING_UP:
+            self.play("swing" + str(randint(1, 4)))
+        elif self.pendulum_state == PendulumState.STABILIZING and not (
+            "stabilizing" in self.last_played_sound
         ):
-            self.get_logger().info("playing")
-
-            p = vlc.MediaPlayer(
-                self.sounds_path + "swing" + str(randint(1, 4)) + ".mp3"
-            )
-            p.play()
-            self.last_played_stamp = self.get_clock().now()
-        
-        if angle < 0.7 and not self.stabilizing:
-            self.stabilizing = True
-            p = vlc.MediaPlayer(
-                self.sounds_path + "stabilizing.mp3"
-            )
-            p.play()
-        
-        if angle < 0.01 and not self.stabilized:
-            self.stabilized = True
-            p = vlc.MediaPlayer(
-                self.sounds_path + "stabilized" + str(randint(1, 3)) + ".mp3"
-            )
-            p.play()
-        
-        if angle > 0.1 and self.stabilized:
-            self.stabilized = False
-            p = vlc.MediaPlayer(
-                self.sounds_path + "scream.mp3"
-            )
-            p.play()
-
-        self.last_cmd = msg.data[0]
+            self.play("stabilizing")
+        elif (
+            self.pendulum_state == PendulumState.STABILIZED
+            and self.get_clock().now() - self.last_played_stamp
+            > self.stabilized_play_time
+        ):
+            self.play("stabilized" + str(randint(1, 4)))
+            self.stabilized_play_time = Duration(seconds=uniform(5, 10))
+        elif self.pendulum_state == PendulumState.MOVED and not (
+            "scream" in self.last_played_sound
+        ):
+            self.play("scream")
 
     def limit_minus_pi_pi(self, angle):
         angle = math.fmod(angle, 2 * math.pi)
-        if (angle > math.pi):
+        if angle > math.pi:
             angle = 2 * math.pi - angle
-        elif (angle < -math.pi):
+        elif angle < -math.pi:
             angle = 2 * math.pi + angle
         return angle
 
     def cb(self, msg: JointState):
         self.current_state = msg
-        self.current_state.position[1] = self.limit_minus_pi_pi(self.current_state.position[1] - math.pi)
+        self.current_state.position[1] = self.limit_minus_pi_pi(
+            self.current_state.position[1] - math.pi
+        )
+
+        angle = math.fabs(self.current_state.position[1])
+        vel = math.fabs(self.current_state.velocity[1])
+
+        if angle > 0.7:
+            self.pendulum_state = PendulumState.SWING_UP
+        elif angle < 0.7 and self.pendulum_state == PendulumState.SWING_UP:
+            self.pendulum_state = PendulumState.STABILIZING
+        elif (
+            angle < 0.01
+            and vel < 0.01
+            and (
+                self.pendulum_state == PendulumState.STABILIZING
+                or self.pendulum_state == PendulumState.MOVED
+            )
+        ):
+            self.pendulum_state = PendulumState.STABILIZED
+        elif angle > 0.01 and self.pendulum_state == PendulumState.STABILIZED:
+            self.pendulum_state = PendulumState.MOVED
 
 
 def main(args=None):
@@ -108,6 +136,3 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
-
-# pulseaudio --kill
-# pulseaudio --start
