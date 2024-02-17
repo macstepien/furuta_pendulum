3d1e88d797e6ba2e7201efdaad10b96492c901f0
Maciej Stępień <maciek1284@outlook.com>
2024-02-17 20:07:11 +0000

    Fix expert - change swing up, instead of using filtered data

--- furuta_pendulum_rl/src/lqr_expert.py
+++ furuta_pendulum_rl/src/lqr_expert.py
@@ -4,55 +4,77 @@ from utils import limit_minus_pi_pi
 
 
 class LQRExpert:
-    def __init__(self) -> None:
-        self.dtheta2_filtered_swingup_ = 0.0
-        self.dtheta1_filtered_ = 0.0
-        self.dtheta2_filtered_ = 0.0
-
-        self.l2_ = 0.096608
-        self.m2_ = 0.009801
+    def __init__(self, n_envs=1) -> None:
         self.g_ = 9.80665
-        self.u_max_ = 0.169
-        self.alpha_swingup_ = 0.01
-        self.alpha_ = 0.1
+
+        self.m1_ = 0.040466
+        self.m2_ = 0.009801
+
+        self.l1_ = 0.05415
+        self.l2_ = 0.096608
+
+        self.L1_ = 0.093525
+        self.L2_ = 0.129
+
+        self.J1_ = 5.96237099e-04
+        self.J2_ = 0.000014790595
+
+        self.J1_hat_ = self.J1_ + self.m1_ * self.l1_ * self.l1_
+        self.J2_hat_ = self.J2_ + self.m2_ * self.l2_ * self.l2_
+
+        self.u_max_ = 0.19
+
         self.K = np.array([0.0, 1.4075, -0.070089, 0.1584])
         self.lqr_transition_angle_ = 0.075
         self.torque_multiplier_ = 0.8
 
     def SwingUpControl(self, obs: np.array):
         theta2 = limit_minus_pi_pi(obs[1])
+        # dtheta1 = obs[2]
+        dtheta2 = obs[3]
 
-        E = 0.5 * self.m2_ * pow(self.l2_, 2) * math.pow(
-            self.dtheta2_filtered_swingup_, 2
-        ) + self.m2_ * self.g_ * self.l2_ * math.cos(theta2)
+        E = 0.5 * self.m2_ * math.pow(self.l2_, 2) * math.pow(
+            dtheta2, 2
+        ) + self.m2_ * self.g_ * self.l2_ * (math.cos(theta2) + 1)
 
-        E0 = self.m2_ * self.g_ * self.l2_
-        x = (E0 - E) * self.dtheta2_filtered_swingup_ * (1 - math.cos(theta2))
+        # Ek1 = 0.5 * math.pow(dtheta1, 2) * (self.J1_hat_)
+        # Ep2 = self.m2_ * self.g_ * self.l2_ * (math.cos(theta2) + 1)
+        # Ek2 = (
+        #     0.5
+        #     * math.pow(dtheta1, 2)
+        #     * (
+        #         self.m2_ * math.pow(self.L2_, 2)
+        #         + self.J2_hat_ * math.pow(math.sin(theta2 + math.pi), 2)
+        #     )
+        #     + 0.5 * math.pow(dtheta2, 2) * self.J2_hat_
+        #     + self.m2_
+        #     * self.L1_
+        #     * self.l2_
+        #     * math.cos(theta2 + math.pi)
+        #     * dtheta1
+        #     * dtheta2
+        # )
 
-        if x > 0.0:
+        # E = Ek1 + Ep2 + Ek2
+        # E = Ep2 + Ek2
+        # E = Ek2
+
+        E0 = 2.0 * self.m2_ * self.g_ * self.l2_
+        x = (E0 - E) * dtheta2 * math.cos(theta2)
+
+        if x < 0.0:
             return -self.u_max_
         else:
             return self.u_max_
 
     def LQRControl(self, obs: np.array):
-        obs[2] = self.dtheta1_filtered_
-        obs[3] = self.dtheta2_filtered_
         return -self.K.dot(obs)
 
     def __call__(self, observations: np.array, states=None, episode_start=None):
         actions = []
 
-        for obs in observations:
-            self.dtheta2_filtered_swingup_ = (
-                self.alpha_swingup_ * obs[3]
-                + (1.0 - self.alpha_swingup_) * self.dtheta2_filtered_swingup_
-            )
-            self.dtheta1_filtered_ = (
-                self.alpha_ * obs[2] + (1.0 - self.alpha_) * self.dtheta1_filtered_
-            )
-            self.dtheta2_filtered_ = (
-                self.alpha_ * obs[3] + (1.0 - self.alpha_) * self.dtheta2_filtered_
-            )
+        for i in range(len(observations)):
+            obs = observations[i]
 
             if math.fabs(limit_minus_pi_pi(obs[1])) < self.lqr_transition_angle_:
                 u = self.LQRControl(obs)
