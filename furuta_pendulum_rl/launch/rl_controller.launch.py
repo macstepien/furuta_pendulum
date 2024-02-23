71e2eedd44f8e4d56f8c72d2226edc6daa5e1f59
Maciej Stępień <maciek1284@outlook.com>
2024-02-23 11:05:50 +0000

    Add command scaling to rl controller

--- furuta_pendulum_rl/launch/rl_controller.launch.py
+++ furuta_pendulum_rl/launch/rl_controller.launch.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 
 from launch import LaunchDescription
+from launch.substitutions import (
+    PathJoinSubstitution,
+)
 
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    furuta_pendulum_rl_controller_node = Node(
+    controller_params = PathJoinSubstitution(
+        [
+            FindPackageShare("furuta_pendulum_rl"),
+            "config",
+            "controller.yaml",
+        ]
+    )
+
+    furuta_pendulum_controller_node = Node(
         package="furuta_pendulum_rl",
         executable="rl_controller_node",
+        parameters=[controller_params],
     )
-    actions = [furuta_pendulum_rl_controller_node]
+
+    actions = [
+        furuta_pendulum_controller_node,
+    ]
 
     return LaunchDescription(actions)
