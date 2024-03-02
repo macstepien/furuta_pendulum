607ebff313c164e4c7314ba815c3f35823cbeb7b
Maciej Stępień <maciek1284@outlook.com>
2024-03-02 12:23:31 +0000

    Update full sac script

--- furuta_pendulum_rl/src/furuta_pendulum_full_sac.py
+++ furuta_pendulum_rl/src/furuta_pendulum_full_sac.py
@@ -6,8 +6,11 @@ from stable_baselines3.common.env_util import make_vec_env
 
 from stable_baselines3.common.evaluation import evaluate_policy
 
+import torch
 
-max_episode_steps = 2000
+torch.set_default_device("cuda")
+
+max_episode_steps = 2500
 register(
     id="FurutaPendulum-v0",
     entry_point="furuta_pendulum_full:FurutaPendulumEnv",
@@ -15,12 +18,12 @@ register(
 )
 
 
-train_model = False
-# train_model = True
+# train_model = False
+train_model = True
 
 if train_model:
     # Parallel environments
-    env = make_vec_env("FurutaPendulum-v0", n_envs=4)
+    env = make_vec_env("FurutaPendulum-v0", n_envs=12)
     # env = gym.make("FurutaPendulum-v0", render_mode="human")
     # model = SAC(
     #     "MlpPolicy",
@@ -29,7 +32,16 @@ if train_model:
     #     tensorboard_log="furuta_pendulum_rl/furuta_pendulum_logs",
     #     policy_kwargs=dict(net_arch=[512, 512, 512]),
     # )
-    model = SAC.load("furuta_pendulum_rl/trained_agents/furuta_pendulum_full_pretrained", env)
+    model = SAC.load(
+        "furuta_pendulum_rl/trained_agents/furuta_pendulum_full_pretrained",
+        env,
+        learning_rate=0.0001,
+        learning_starts=200_000,
+        use_sde=True,
+        sde_sample_freq = 1,
+        # train_freq=10,
+    )
+    # model = SAC.load("furuta_pendulum_rl/trained_agents/furuta_pendulum_full", env)
     # model = PPO.load(
     #     # "furuta_pendulum_rl/trained_agents/furuta_pendulum_full_pretrained",
     #     "furuta_pendulum_rl/trained_agents/furuta_pendulum_full",
@@ -41,7 +53,7 @@ if train_model:
 
     reward_before, _ = evaluate_policy(model, env, n_eval_episodes=10)
 
-    model.learn(total_timesteps=100_000, progress_bar=True)
+    model.learn(total_timesteps=300_000, progress_bar=True)
     model.save("furuta_pendulum_rl/trained_agents/furuta_pendulum_full")
 
     reward_after, _ = evaluate_policy(model, env, n_eval_episodes=10)
@@ -58,9 +70,10 @@ else:
     env = gym.make("FurutaPendulum-v0", render_mode="human")
     # model = PPO.load("furuta_pendulum_rl/trained_agents/furuta_pendulum_full")
     # model = PPO.load("furuta_pendulum_rl/trained_agents/furuta_pendulum_full_pretrained")
-    # model = SAC.load("furuta_pendulum_rl/trained_agents/furuta_pendulum_full_pretrained")
-    model = SAC.load("furuta_pendulum_rl/trained_agents/furuta_pendulum_full")
+    model = SAC.load(
+        "furuta_pendulum_rl/trained_agents/furuta_pendulum_full_pretrained"
+    )
+    # model = SAC.load("furuta_pendulum_rl/trained_agents/furuta_pendulum_full")
 
     reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
     print(f"Reward: {reward}")
-
